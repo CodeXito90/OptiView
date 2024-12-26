99fe1d919610f9b533846a715cf7d11f7d1a1107
jama mohamed abdi <j.m.abdi@hotmail.com>
2024-12-26 14:50:19 +0100

    #Validated model entities and updtaed frontend UI with following additions to card container..... ADD, DEL, SEND, STATS #MachineDataDto- Realistisk maskindata , Visuell förhandsgranskning av data som kommer att skickas. Realtidsdata med slumpmässiga värden inom realistiska intervall

---
+++ OptiView.Application/DTOs/MachineDataDto.cs
@@ -0,0 +1,9 @@
+public class MachineDataDto
+{
+    public double Temperature { get; set; }
+    public double Pressure { get; set; }
+    public double Speed { get; set; }
+    public double PowerUsage { get; set; }
+    public int ProductionCount { get; set; }
+    public DateTime Timestamp { get; set; }
+} 
--- OptiView.Application/DTOs/MachineDto.cs
+++ OptiView.Application/DTOs/MachineDto.cs
@@ -4,16 +4,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace OptiView.Application.DTOs
 {
     public class MachineDto
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
+        public string Id { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Status is required")]
         public MachineStatus Status { get; set; }
+
         public DateTime LastUpdated { get; set; }
-        public string Description { get; set; }
+
+        [StringLength(200, ErrorMessage = "Description cannot exceed 200 characters")]
+        public string Description { get; set; } = string.Empty;
+
         public string StatusDisplay => Status.ToString();
         public string LastUpdatedDisplay => LastUpdated.ToString("dd-MMM-yyyy, HH:mm");
         public string StatusColor => Status switch
