fc5b2f2aa42c6669a629d389c13bcc9359a1768f
jama mohamed abdi <j.m.abdi@hotmail.com>
2025-01-05 15:34:20 +0100

    created landing page for our homes section, css styling wont work in our wwwroot app.css or home.razor.css It seems only in home.razor files will styling have effect?.. need to have a closer look. Added reusuable componenets, features and statscards.

---
+++ OptiView.Presentation/Components/Pages/Home.razor.cs
@@ -0,0 +1,37 @@
+﻿using OptiView.Application.DTOs;
+using OptiView.Domain.Entities;
+using System.Linq;
+
+namespace OptiView.Presentation.Components.Pages
+{
+    public partial class Home
+    {
+        private List<MachineDto> machines;
+
+        protected override async Task OnInitializedAsync()
+        {
+            var machineEntities = await MachineService.GetAllMachinesAsync();
+            machines = machineEntities.Select(machine => new MachineDto
+            {
+                Id = machine.Id,
+                Name = machine.Name,
+                Status = machine.Status,
+                LastUpdated = machine.LastUpdated,
+                Description = machine.Description
+            }).ToList();
+        }
+
+        private void NavigateToMachines()
+        {
+            NavigationManager.NavigateTo("/machines");
+        }
+
+        private string GetPercentage(Func<MachineDto, bool> predicate)
+        {
+            if (machines == null || !machines.Any())
+                return "0";
+
+            return Math.Round((double)machines.Count(predicate) / machines.Count * 100, 1).ToString("0.#");
+        }
+    }
+}
